64a880c779eb0843bb4f01965bfd98888707c39d
huGgW <whjoon0225@naver.com>
2025-04-27 23:00:11 +0900

    Extract URL Path

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
-#[allow(unused_imports)]
 use std::net::TcpListener;
-use std::{io::Write, net::TcpStream};
+use std::{
+    io::{BufRead, BufReader, Write},
+    net::TcpStream,
+};
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -29,9 +31,29 @@ fn main() {
     }
 }
 
-fn handle_connection(stream: &mut TcpStream) -> Result<(), std::io::Error> {
-    write!(stream, "HTTP/1.1 200 OK\r\n\r\n")?;
-    stream.flush()?;
+fn handle_connection(stream: &mut TcpStream) -> Result<(), Box<dyn std::error::Error>> {
+    let read_stream = stream.try_clone()?;
+    let mut reader = BufReader::new(read_stream);
+    let mut buf = Vec::new();
+    reader.read_until(b'\n', &mut buf)?;
+    if !(buf.get(buf.len() - 2).is_some_and(|b| b.eq(&b'\r'))) {
+        return handle_unknown(stream);
+    }
+
+    let req_line = String::from_utf8(buf)?;
+    let req_line_args = req_line.split(' ').collect::<Vec<_>>();
+    if !(req_line_args.first().is_some_and(|&s| s.eq("GET"))
+        && req_line_args.get(1).is_some_and(|&s| s.eq("/")))
+    {
+        return handle_unknown(stream);
+    }
+
+    stream.write_all(b"HTTP/1.1 200 OK\r\n\r\n")?;
+    Ok(())
+}
+
+fn handle_unknown(stream: &mut TcpStream) -> Result<(), Box<dyn std::error::Error>> {
+    stream.write_all(b"HTTP/1.1 404 Not Found\r\n\r\n")?;
 
     Ok(())
 }
