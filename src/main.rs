240e630e46c1a5920a913734c890e2e6e5a371c1
huGgW <whjoon0225@naver.com>
2025-04-27 21:38:57 +0900

    Response with 200

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #[allow(unused_imports)]
 use std::net::TcpListener;
+use std::{io::Write, net::TcpStream};
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -9,12 +10,28 @@ fn main() {
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(mut stream) => {
                 println!("accepted new connection");
+
+                if let Err(e) = handle_connection(&mut stream) {
+                    println!("handle connection error: {}", e);
+                }
+
+                if let Err(e) = stream.shutdown(std::net::Shutdown::Both) {
+                    println!("shutdown error: {}", e);
+                }
+                println!("connection closed");
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
+}
+
+fn handle_connection(stream: &mut TcpStream) -> Result<(), std::io::Error> {
+    write!(stream, "HTTP/1.1 200 OK\r\n\r\n")?;
+    stream.flush()?;
+
+    Ok(())
 }
