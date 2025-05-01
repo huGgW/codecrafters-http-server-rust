f8ffbc9c4ff81d4428b947e216127afb598a58a8
huGgW <whjoon0225@naver.com>
2025-05-01 21:40:53 +0900

    Return a file

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 mod request;
 mod response;
 
+use clap::{arg, Parser};
 use request::Request;
 use response::{Response, Status};
 use std::collections::HashMap;
@@ -11,19 +12,28 @@ use std::{
     net::TcpStream,
 };
 
+#[derive(Parser, Clone)]
+struct Args {
+    #[arg(long)]
+    directory: Option<String>,
+}
+
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
+
+    let args = Args::parse();
 
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
     let mut thread_handles = Vec::new();
 
     for stream in listener.incoming() {
+        let args = args.clone();
         let handle = thread::spawn(move || match stream {
             Ok(mut stream) => {
                 println!("accepted new connection");
 
-                if let Err(e) = handle_connection(&mut stream) {
+                if let Err(e) = handle_connection(&mut stream, &args) {
                     println!("handle connection error: {}", e);
                 }
 
@@ -41,29 +51,33 @@ fn main() {
     }
 }
 
-fn handle_connection(stream: &mut TcpStream) -> Result<(), std::io::Error> {
+fn handle_connection(stream: &mut TcpStream, args: &Args) -> Result<(), std::io::Error> {
     let read_stream = stream.try_clone()?;
     let mut reader = BufReader::new(read_stream);
     let request = Request::parse(&mut reader)?;
 
-    let handler = router(&request);
+    let handler = router(&request, &args);
     let response = handler(&request).unwrap_or_else(|_| unknwon_handler(&request).unwrap());
 
     stream.write_all(&response.to_bytes())?;
     Ok(())
 }
 
-fn router(request: &Request) -> fn(&Request) -> Result<Response, std::io::Error> {
+fn router(
+    request: &Request,
+    args: &Args,
+) -> Box<dyn FnOnce(&Request) -> Result<Response, std::io::Error>> {
     // if not GET, 404
     if request.start_line.method != "GET" {
-        return unknwon_handler;
+        return Box::new(unknwon_handler);
     }
 
     match request.start_line.path.as_str() {
-        "/" => default_handler,
-        s if s.starts_with("/echo") => echo_handler,
-        u if u.starts_with("/user-agent") => user_agent_handler,
-        _ => unknwon_handler,
+        "/" => Box::new(default_handler),
+        s if s.starts_with("/echo") => Box::new(echo_handler),
+        u if u.starts_with("/user-agent") => Box::new(user_agent_handler),
+        f if f.starts_with("/files") => Box::new(files_handler_provider(args)),
+        _ => Box::new(unknwon_handler),
     }
 }
 
@@ -123,6 +137,52 @@ fn user_agent_handler(request: &Request) -> Result<Response, std::io::Error> {
         ]),
         body: user_agent.as_bytes().to_vec(),
     })
+}
+
+fn files_handler_provider(
+    args: &Args,
+) -> impl FnOnce(&Request) -> Result<Response, std::io::Error> {
+    let directory_arg = args.directory.clone();
+
+    move |request: &Request| -> Result<Response, std::io::Error> {
+        let directory = match directory_arg {
+            Some(d) => d.clone(),
+            None => {
+                return Err(std::io::Error::new(
+                    io::ErrorKind::NotFound,
+                    "directory arg not given",
+                ))
+            }
+        };
+
+        let filename = match request.start_line.path[1..].split_once('/') {
+            Some(("files", filename)) => filename,
+            _ => {
+                return Err(std::io::Error::new(
+                    io::ErrorKind::InvalidInput,
+                    "file path is not valid",
+                ))
+            }
+        };
+
+        let file = std::fs::read(directory + filename)?;
+
+        Ok(Response {
+            status: Status {
+                version: String::from("1.1"),
+                status_code: 200,
+                status: String::from("OK"),
+            },
+            headers: HashMap::from([
+                (
+                    String::from("Content-Type"),
+                    String::from("application/octet-stream"),
+                ),
+                (String::from("Content-Length"), file.len().to_string()),
+            ]),
+            body: file,
+        })
+    }
 }
 
 fn default_handler(_: &Request) -> Result<Response, std::io::Error> {
