caa14fd644e8cf3d2d7f44346f6ec39ac22fb476
huGgW <whjoon0225@naver.com>
2025-05-09 01:42:00 +0900

    Multiple compression schemes

--- src/middleware.rs
+++ src/middleware.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 use crate::{handler::Handler, request::Request, response::Response};
 
 pub type Middleware = dyn FnOnce(Box<Handler>) -> Box<Handler>;
@@ -8,22 +10,23 @@ pub fn gzip_compress_middleware(handler: Box<Handler>) -> Box<Handler> {
     const GZIP_ENCODING: &str = "gzip";
 
     let wrapped = move |request: &Request| -> Result<Response, std::io::Error> {
-        let accept_encoding = request
+        let accept_encodings = request
             .headers
-            .get(ACCEPT_ENCODING_HEADER.to_lowercase().as_str());
+            .get(ACCEPT_ENCODING_HEADER.to_lowercase().as_str())
+            .map(|s| s.split(',').map(|s| s.trim()).collect::<HashSet<_>>())
+            .unwrap_or_default();
 
         let response_result = handler(request);
         match response_result {
-            Ok(mut response) => match accept_encoding {
-                Some(&ref e) if e == GZIP_ENCODING => {
-                    _ = response.headers.insert(
+            Ok(mut response) => {
+                if accept_encodings.contains(GZIP_ENCODING) {
+                    response.headers.insert(
                         CONTENT_ENCODING_HEADER.to_string(),
                         GZIP_ENCODING.to_string(),
                     );
-                    Ok(response)
                 }
-                _ => Ok(response),
-            },
+                Ok(response)
+            }
             Err(e) => Err(e),
         }
     };
